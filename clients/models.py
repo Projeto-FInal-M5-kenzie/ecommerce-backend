4d38a803ccaec72838672a6290d890b2f534e116
Gutemberg Lamark <gtmbrglamark@gmail.com>
2023-01-02 14:22:19 -0300

    feat: adicionando uuid na model client

--- clients/models.py
+++ clients/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser 
+import uuid
 
 # Create your models here.
 class Client(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     email = models.EmailField(unique=True, max_length=150)
     cpf = models.CharField(max_length=11, unique=True)
     phone = models.CharField(max_length=13)
