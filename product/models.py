3c794bad87dee4354283bb01d0d84f185d563205
João Gonçalves <joaogoncalves@MacBook-Air-de-Joao.local>
2023-01-02 13:52:13 -0300

    feat-modelProduct

--- product/models.py
+++ product/models.py
@@ -7,3 +7,13 @@ class Category_product(models.Model):
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+
+class Product(models.Model):
+    name_product = models.CharField(max_length=150, null=True)
+    description = models.TextField(null=True)
+    is_active = models.BooleanField(default=True)
+    price = models.FloatField(2, null=True)
+    stock = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now_add=True, editable=False)
