5201c4eb4fe0c33beef4bf1daa807e19bc4cf05e
GeovaneSO <oliveira.5geovane@outlook.com>
2023-01-04 03:20:55 -0300

    feat: OrderProducterializer created

--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import OrderProduct
+from django.shortcuts import get_object_or_404
+from .serializers import OrderProductSerializer
 
-# Create your views here.
+
+class OrderProductView(generics.ListCreateAPIView):
+    
+    authentication_classes = [JWTAuthentication]
+    
+    queryset = OrderProduct.objects.all()
+    serializer_class = OrderProductSerializer
+
+    def perform_create(self, serializer):
+
+        product_id = self.kwargs["product_id"]
+        
+        product_obj = get_object_or_404(OrderProduct, pk=product_id)
+        
+        serializer.save(clients=self.request.user, product=product_obj,**self.request.data)
+        
