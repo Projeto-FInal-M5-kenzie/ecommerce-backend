ae8ce84fe7f33762c234b40576fa2ba6e65297f2
GeovaneSO <oliveira.5geovane@outlook.com>
2023-01-05 00:27:34 -0300

    feat: adding route for list product in specific category

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("product/", views.ProductView.as_view()),
-    path("product/<uuid:category_id>/", views.ProductCategoryView.as_view()),
+    path("product/category/<uuid:category_id>/", views.ProductCategoryView.as_view()),
     # path("product/<uuid:product_id>/restore/", views.RestoreCategoryView.as_view()),
 ]
