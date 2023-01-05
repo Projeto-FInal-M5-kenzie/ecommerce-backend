dcaa571c3850fc6a0cd6fed543178667901bcbaf
GeovaneSO <oliveira.5geovane@outlook.com>
2023-01-04 22:43:48 -0300

    feat: creating logic from creation product

--- products/views.py
+++ products/views.py
@@ -1,23 +1,50 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from .models import OrderProduct
+from .models import OrderProduct, Product
 from django.shortcuts import get_object_or_404
-from .serializers import OrderProductSerializer
+from .serializers import OrderProductSerializer, ProductSerializer
+from categories_products.models import Category_product
+import ipdb
+
+
+class ProductView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(**self.request.data)
+
+
+class ProductCategoryView(generics.ListAPIView):
+   
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_object(self):
+       
+        category_id = self.kwargs["category_id"]
+      
+        category_obj = get_object_or_404(Category_product, pk=category_id)
+        
+        products_list = Product.objects.filter(category=category_obj)
+       
+        return products_list
 
 
 class OrderProductView(generics.ListCreateAPIView):
-    
+
     authentication_classes = [JWTAuthentication]
-    
+
     queryset = OrderProduct.objects.all()
     serializer_class = OrderProductSerializer
 
     def perform_create(self, serializer):
 
         product_id = self.kwargs["product_id"]
-        
+
         product_obj = get_object_or_404(OrderProduct, pk=product_id)
-        
-        serializer.save(clients=self.request.user, product=product_obj,**self.request.data)
-        
+
+        serializer.save(
+            clients=self.request.user, product=product_obj, **self.request.data
+        )
