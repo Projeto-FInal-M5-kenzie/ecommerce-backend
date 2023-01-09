c561ddc50eb9212c2a7eec176c23e7db7181f06f
GeovaneSO <oliveira.5geovane@outlook.com>
2023-01-08 23:58:14 -0300

    fix: fixnig conflicts

--- products/views.py
+++ products/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import OrderProduct, Product
-from django.shortcuts import get_object_or_404
 from .serializers import ProductSerializer, OrderProductSerializer
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
@@ -21,17 +20,11 @@ class ProductView(generics.ListCreateAPIView):
         route_parameter = self.request.GET.get("product_name")
         if route_parameter:
 
-<<<<<<< HEAD
             return Product.objects.filter(product_name__icontains=route_parameter)
 
         return super().get_queryset()
-=======
-class ProductCategoryView(APIView):
-    def get(self, req: Request, category_id: str) -> Response:
->>>>>>> 3c378ec (feat: refactoring order product)
 
 
-<<<<<<< HEAD
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -42,11 +35,6 @@ class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
         product_obj = get_object_or_404(Product, pk=product_id)
 
         return product_obj
-=======
-        serializer = ProductSerializer(products_list, many=True)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
->>>>>>> 3c378ec (feat: refactoring order product)
 
 
 class OrderProductView(generics.ListCreateAPIView):
